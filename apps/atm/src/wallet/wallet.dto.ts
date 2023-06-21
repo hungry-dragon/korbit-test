d73066a2206516650d8aec1d158dab457c09090b
justin heo <koreanwiseman@gmail.com>
2023-06-22 00:30:03 +0900

    요구사항 공통을 빼먹었음 ㅠㅠ (#1)

--- apps/atm/src/wallet/wallet.dto.ts
+++ apps/atm/src/wallet/wallet.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiResponseProperty } from '@nestjs/swagger';
 import { IsInt, Min } from 'class-validator';
 
 export class PostWalletCommand {
@@ -10,4 +10,21 @@ export class PostWalletCommand {
   @IsInt()
   @Min(0)
   balance: number;
+}
+
+export class WalletDto {
+  @ApiResponseProperty({ example: 'uuid' })
+  walletId: string;
+
+  @ApiResponseProperty({ example: 100 })
+  balance: number;
+
+  @ApiResponseProperty({ example: '2023-01-01T00:00:00.000Z' })
+  createdAt: Date;
+
+  @ApiResponseProperty({ example: '2023-01-01T00:00:00.000Z' })
+  updatedAt: Date;
+
+  @ApiResponseProperty({ example: -100 })
+  notReflectedAmount: number;
 }
--- apps/atm/src/wallet/wallet.service.ts
+++ apps/atm/src/wallet/wallet.service.ts
@@ -3,16 +3,40 @@ import { Repository } from 'typeorm';
 import { WalletEntiry } from '../entity/Wallet.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { v4 as uuidV4 } from 'uuid';
+import { BankingEntity } from '../entity/Banking.entity';
+import { BankingStatus } from '../enum/BankingStatus';
+import { WalletDto } from './wallet.dto';
+import { BankingType } from '../enum/BankingType';
 
 @Injectable()
 export class WalletService {
   constructor(
     @InjectRepository(WalletEntiry)
     private readonly wallerRepository: Repository<WalletEntiry>,
+    @InjectRepository(BankingEntity)
+    private readonly bankingRepository: Repository<BankingEntity>,
   ) {}
 
-  public findOne(walletId: string) {
-    return this.wallerRepository.findOneBy({ walletId });
+  public async findOne(walletId: string): Promise<WalletDto> {
+    const wallet = await this.wallerRepository.findOneBy({ walletId });
+    if (!wallet) {
+      return null;
+    }
+    const bankings = await this.bankingRepository.find({
+      where: {
+        walletId,
+        bankingStatus: BankingStatus.IN_PROGRESS,
+      },
+      select: ['amount', 'bankingType'],
+    });
+    return {
+      ...wallet,
+      notReflectedAmount: bankings.reduce((acc, x) => {
+        return (
+          acc + (x.bankingType === BankingType.DEPOSIT ? x.amount : -x.amount)
+        );
+      }, 0),
+    };
   }
 
   public save(balance: number) {
